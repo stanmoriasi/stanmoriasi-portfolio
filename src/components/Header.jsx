3d847d2a80f9eb08bf7841835536ba26ba0b2919
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-08 01:36:41 -0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,9 @@
-import { NavLink } from "react-router-dom";
+import Navigation from "./Navigation";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul className="list-none flex border w-[60vw]">
-          <li> <NavLink className="nav-link me-3" to="/">Home</NavLink></li>
-          <li> <NavLink className="nav-link me-3" to="/profile">My Profile</NavLink></li>
-          <li> <NavLink className="nav-link me-3" to="/aboutme">About me</NavLink></li>
-          <li> <NavLink className="nav-link me-3" to="/projects">Projects</NavLink></li>
-          <li> <NavLink className="nav-link me-3" to="/contactme">Contact Me</NavLink></li>
-        </ul>
-      </nav>
+    <header className="w-full flex item-center justify-end border">
+      <Navigation />
     </header>
   );
 };
