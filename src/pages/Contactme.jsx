2d1b9aa6e387ea92cf5d491ae67a53fd024bfc3d
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-13 02:46:29 -0700

    add resume

--- src/pages/Contactme.jsx
+++ src/pages/Contactme.jsx
@@ -1,23 +1,23 @@
 function Contact() {
 
     return (
-      <div className="flex flex-col items-center justify-center p-6" id="contactme">
+      <div className="flex flex-col items-center" id="contactme" >
       <h1 className="text-4xl font-bold text-white mb-8">Contact Me</h1>
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md ">
+      <div className="bg-white p-12 rounded-lg shadow-lg">
         <form>
-          <div className="mb-4 ">
+          <div className="mb-6">
             <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name</label>
-            <input type="text" id="name" name="name" required className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500" />
+            <input type="text" id="name" name="name" required className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500" />
           </div>
-          <div className="mb-4">
+          <div className="mb-6">
             <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
-            <input type="email" id="email" name="email" required className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500" />
+            <input type="email" id="email" name="email" required className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500" />
           </div>
-          <div className="mb-4">
+          <div className="mb-6">
             <label htmlFor="details" className="block text-gray-700 font-bold mb-2">Details</label>
-            <textarea id="details" name="details" rows="4" required className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"></textarea>
+            <textarea id="details" name="details" rows="6" required className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"></textarea>
           </div>
-          <button type="submit" className="w-full bg-green-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">Submit</button>
+          <button type="submit" className="w-full bg-green-500 text-white font-bold py-3 px-4 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">Submit</button>
         </form>
       </div>
     </div>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,15 @@ function Resume() {
         <div className="contact-container" id="resume">
             <h1 className="text-4xl font-bold text-white">View my Resume</h1>
             <div className="contact-info">
-                
+            <div className="contact-info">
+        <a
+          href="/assets/stanmoriasi.pdf"
+          download="Stan_Moriasi_Resume.pdf"
+          className="text-green-500 underline mt-4 block"
+        >
+          View in PDF format
+        </a>
+      </div>
                 </div>
                 </div>
     );
