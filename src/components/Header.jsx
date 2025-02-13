d97f1bc777776a556571fb86ccc1b275e632681f
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-13 02:46:03 -0700

    add styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navigation from "./Navigation";
 const Header = () => {
   return (
     <>
-    <header className="w-full flex item-center justify-end fixed top-0 left-0 z-10 bg-red-800">
+    <header className="w-full flex item-center justify-end fixed top-0 left-0 z-10">
       <Navigation />      
     </header>
 
