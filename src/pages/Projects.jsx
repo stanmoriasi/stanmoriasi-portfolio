4751c75c2a3b154e3d295b90ffc1069d20fcd421
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-14 22:12:25 -0700

    update projects, update images

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,9 +2,9 @@ import Project from "../components/Project";
 function Projects() {
   const projects = [
     { name: "Employee Tracker", url: "https://github.com/stanmoriasi/employee-tracker-using-nodejs", images: ["/assets/images/employeeTrackerCLI.png"], description: "This project is used to perform basic operations on an employee database. The project utilizes Postgres database on the backend, and Inquirer package for generating user prompts on a command line interface.." , stack: [ "Javascript","Nodejs", "Postgres"] },
-    { name: "5 Day Weather Forecast", url: "https://github.com/stanmoriasi/five-day-weather-dashboard", images: ["/assets/images/employeeTrackerCLI.png"], description: "This project is a weather forecast application utilizing the Open API to show a day forecast for a city that is entered.",stack: ["Typescript", "Nodejs", "HTML5"] },
-    { name: "Portfolio", url: "example.com", images: ["/assets/images/employeeTrackerCLI.png"], description: "My personal portfolio website." , stack: ["React", "Javascript", "MongoDB"]},
-    { name: "E-commerce", url: "https://github.com/stanmoriasi/readme-generator", images: ["/assets/images/employeeTrackerCLI.png"], description: "This project is used to help any developer a generate professional README.md file", stack: ["Javascript","Typescript","ReactJS"] },
+    { name: "5 Day Weather Forecast", url: "https://github.com/stanmoriasi/five-day-weather-dashboard", images: ["/assets/images/weatherforecast.png"], description: "This project is a weather forecast application utilizing the Open API to show a day forecast for a city that is entered.",stack: ["Typescript", "Nodejs", "HTML5"] },
+    { name: "Personal Portfolio", url: "https://github.com/stanmoriasi/stanmoriasi-portfolio", images: ["/assets/images/portfolio.png"], description: "My personal portfolio website." , stack: ["React", "Javascript", "ExpressJS"] },
+    { name: "README.MD Generator", url: "https://github.com/stanmoriasi/readme-generator", images: ["/assets/images/readme.png"], description: "This project is used to help any developer a generate professional README.md file. Using the command line, a user is prompted to enter details for a given project then the answers are formated in markdown to generate a read me file.", stack: ["Javascript","Typescript","NodeJS"] },
   ];
 
   return (
